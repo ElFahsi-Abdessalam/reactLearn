7abd4dfc9ab19555ac974e90c18e304b0a806857
ELFahsi-Abdessalam <abdessalamelfahsi@gmail.com>
2023-01-27 13:07:24 +0100

    skill implementation and image for the website

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import {Container, Row,Col} from "react-bootstrap";
+import {Container, Row, Col, Nav, Tab} from "react-bootstrap";
 
 export const Projects= () =>{
 
@@ -25,6 +25,36 @@ export const Projects= () =>{
                 <Row>
                     <Col>
                         <h2>Projects</h2>
+                        <p>Lorem ipsum is simply dummy text</p>
+                        <Tab.Container id='projects-tabs' defaultActiveKey="fisrt" >
+                        <Nav variant="pills" defaultActiveKey="/home">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="seconde">Tab two</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">Tab three</Nav.Link>
+                            </Nav.Item>
+
+                        </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey ="first">
+                                    <Row>
+                                        {
+                                            projects.map((project,index) => {
+                                                return(
+                                                    <p>{project.title}</p>
+                                        )
+                                        })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey ="seconde">Loremp</Tab.Pane>
+                                <Tab.Pane eventKey ="third">lorem</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
