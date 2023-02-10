298b051796f0654887ec7e6e4377402da7f1d143
maciejrolecki <maciej.role@gmail.com>
2023-02-10 22:03:55 +0000

    page modifications, colors, and add contact form

--- server1.js
+++ server1.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const path = require("path");
 // server used to send send emails
 const app = express();
 app.use(cors());
@@ -11,50 +10,47 @@ app.use("App.js", router);
 app.use(express.static("src"));
 app.get("/*",function (req,res) {
   res.sendFile(path.join(__dirname, './src', 'App.js'));
-  //res.sendFile(path.resolve("public","index.html"));
- //res.sendFile("./src/App.js");
 });
-//app.use(express.static("server1.js"))
 app.listen(3000, () => console.log("Server Running"));
-//console.log(process.env.EMAIL_USER);
-//console.log(process.env.EMAIL_PASS);
-//
-//const contactEmail = nodemailer.createTransport({
-//  service: 'gmail',
-//  auth: {
-//    user: "********@gmail.com",
-//    pass: ""
-//  },
-//});
-//
-//contactEmail.verify((error) => {
-//  if (error) {
-//    console.log(error);
-//  } else {
-//    console.log("Ready to Send");
-//  }
-//});
-//
-//router.post("/contact", (req, res) => {
-//  const name = req.body.firstName + req.body.lastName;
-//  const email = req.body.email;
-//  const message = req.body.message;
-//  const phone = req.body.phone;
-//  const mail = {
-//    from: name,
-//    to: "********@gmail.com",
-//    subject: "Contact Form Submission - Portfolio",
-//    html: `<p>Name: ${name}</p>
-//           <p>Email: ${email}</p>
-//           <p>Phone: ${phone}</p>
-//           <p>Message: ${message}</p>`,
-//  };
-//  contactEmail.sendMail(mail, (error) => {
-//    if (error) {
-//      res.json(error);
-//    } else {
-//      res.json({ code: 200, status: "Message Sent" });
-//    }
-//  });
-//});
-//
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: 'mail.gandi.net',
+  port: '465',
+  auth: {
+    user: "admin@mrolecki.com",
+    pass: "arolkaM1998@#"
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "admin@mrolecki.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import colorSharp2 from "../assets/img/color-sharp2.png"
 import projImg1 from "../assets/img/project-img1.png"
 import projImg2 from "../assets/img/project-img1.png"
 import projImg3 from "../assets/img/project-img3.png"
+import header from "../assets/img/header-img.svg";
+
 export const Projects= () =>{
 
     const projects =[
@@ -92,7 +94,7 @@ export const Projects= () =>{
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-right" src={colorSharp2}/>
+            
         </section>
     )
 }
